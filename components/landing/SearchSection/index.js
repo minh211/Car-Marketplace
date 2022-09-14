e9cf781c71851c9f9480b4b77da9a7346eccb0c4
MinhL <leminh210199@gmail.com>
2022-09-14 21:33:31 +0700

    fix: dropdown - re-check components

--- components/landing/SearchSection/index.js
+++ components/landing/SearchSection/index.js
@@ -4,13 +4,26 @@ import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { useState } from 'react';
+import {Divider, OutlinedInput, InputLabel, Checkbox, FormControlLabel, FormLabel, FormGroup} from "@mui/material";
 
 const SearchSection = () => {
+    const [checkedState, setCheckedState] = useState({
+        bus1: true,
+        bus2: false,
+        bus3: false,
+    })
     const [carType, setCarType] = useState();
     const [priceRange, setPriceRange] = useState();
     const [vehicleType, setVehicleType] = useState();
     const [active, setActive] = useState();
 
+    const {bus1,bus2,bus3} = checkedState;
+    const handleCheckedChange = (event) => {
+        setCheckedState({
+            ...checkedState,
+            [event.target.name]: event.target.checked,
+        });
+    };
     const handleChangeCarType = event => {
         setCarType(event.target.value);
     };
@@ -84,8 +97,8 @@ const SearchSection = () => {
                             onChange={handleChangePriceRange}
                             onOpen={() => setActive('priceRange')}
                             renderValue={value => `${value}`}
-                            defaultValue="New Cars"
-                            className="h-[50px] !font-[14px]"
+                            defaultValue="$10,0000 - $100,0000"
+                            className="h-[50px] !font-[14px] rounded-[15px]"
                             sx={{
                                 '& .MuiInputBase-input': {
                                     fontSize: '14px',
@@ -96,13 +109,21 @@ const SearchSection = () => {
                                 }
                             }}
                         >
-                            <MenuItem value="New Car (Authorised Dealer)">
-                                New Car (Authorised Dealer)
-                            </MenuItem>
-                            <MenuItem value="New Car (Parallel Importer)">
-                                New Car (Parallel Importer)
-                            </MenuItem>
-                            <MenuItem value="Used Cars">Used Cars</MenuItem>
+                            <div className={"mt-[24px] mb-[16px] mx-[24px] font-[600]"}>Price Range</div>
+                            <div className={"flex justify-center min-w-[400px] mt-[16px]"}>
+                                <MenuItem className={"gap-[10px]"}>
+                                    <FormControl>
+                                        <InputLabel htmlFor="component-outlined">Min</InputLabel>
+                                        <OutlinedInput className={"max-w-[160px]"}label="Min" id="component-outlined" defaultValue="S$ 20,000" value={priceRange} onChange={handleChangePriceRange} />
+                                    </FormControl>
+                                    <Divider className={"w-[8px] border-[#232323]"}/>
+                                    <FormControl>
+                                        <InputLabel htmlFor="component-outlined">Max</InputLabel>
+                                        <OutlinedInput className={"max-w-[160px]"} label="Max" id="component-outlined" defaultValue="S$ 500,000" value={priceRange} onChange={handleChangePriceRange} />
+                                    </FormControl>
+                                </MenuItem>
+                            </div>
+                            <Divider className={" border-[#E3E3E3] flex justify-between items-center"}/>
                         </Select>
                     </FormControl>
                     <div className="border-[3px] border-dash border-[#F1F1F1] h-[60px] rounded-[20px]" />
@@ -132,13 +153,30 @@ const SearchSection = () => {
                                 }
                             }}
                         >
-                            <MenuItem value="New Car (Authorised Dealer)">
-                                +10 More
-                            </MenuItem>
-                            <MenuItem value="New Car (Parallel Importer)">
-                                +10 More
-                            </MenuItem>
-                            <MenuItem value="Used Cars">+10 More</MenuItem>
+                            <FormLabel className={"mt-[24px] mb-[16px] mx-[24px] font-[600]"} component="legend">Vehicle Type</FormLabel>
+                            <FormGroup className={"flex justify-center min-w-[473px] min-w-[350px] mt-[16px]"}>
+                                <FormControlLabel
+                                    className={"ml-[10px]"}
+                                    control={
+                                        <Checkbox checked={bus1} onChange={handleCheckedChange} name="bus1" />
+                                    }
+                                    label="Bus"
+                                />
+                                <FormControlLabel
+                                    className={"ml-[10px]"}
+                                    control={
+                                        <Checkbox checked={bus2} onChange={handleCheckedChange} name="bus2" />
+                                    }
+                                    label="Bus2"
+                                />
+                                <FormControlLabel
+                                    className={"ml-[10px]"}
+                                    control={
+                                        <Checkbox checked={bus3} onChange={handleCheckedChange} name="bus3" />
+                                    }
+                                    label="Bus3"
+                                />
+                            </FormGroup>
                         </Select>
                     </FormControl>
                 </div>
